d93465b64285f0bfaf21c0bca05f54c29398b754
Nhlelo Mngadi <NMngadi@jhb.dvt.co.za>
2020-03-27 10:25:36 +0200

    Add Delete Functionality

--- src/reducers/shopping.reducer.ts
+++ src/reducers/shopping.reducer.ts
@@ -2,13 +2,14 @@ import {
   ShoppingActionTypes,
   ShoppingAction
 } from "../actions/shopping.actions";
-import { ShoppingItem } from "../models/shopping-item.model";
+import { ShoppingItem } from 'src/models/shopping-item.model';
+
 
 const initialState: Array<ShoppingItem> = [
   {
     id: "1775935f-36fd-467e-a667-09f95b917f6d",
     label: "Diet Coke",
-    completed: true
+    completed:false
   }
 ];
 
@@ -19,6 +20,8 @@ export function ShoppingReducer(
   switch (action.type) {
     case ShoppingActionTypes.ADD_ITEM:
       return [...state, action.payload];
+    case ShoppingActionTypes.DELETE_ITEM:
+      return state.filter(item => item.id !== action.payload);
     default:
       return state;
   }
